c3588a5aaabdd8a36cdf489f18c8e35904b0e606
dakosonogov <dakosonogov@gmail.com>
2022-08-24 18:06:28 +0300

    Lesson 10 homework - add JUnit case methods

--- src/lesson10/FridayImInLoveTest.java
+++ src/lesson10/FridayImInLoveTest.java
@@ -15,14 +15,50 @@ class FridayImInLoveTest {
     public static final String BT_RESULT_4 = "Покинуть бар";
 
     @Test
-    public void testFridayCheck() {
+    public void testFridayCheckFirst() {
         String actual = FridayImInLove.fridayCheck(true, true);
         assertEquals(FC_RESULT_1, actual);
     }
 
     @Test
-    public void testBarTrip() {
+    public void testFridayCheckSecond() {
+        String actual = FridayImInLove.fridayCheck(true, false);
+        assertEquals(FC_RESULT_2, actual);
+    }
+
+    @Test
+    public void testFridayCheckThird() {
+        String actual = FridayImInLove.fridayCheck(false, true);
+        assertEquals(FC_RESULT_3, actual);
+    }
+
+    @Test
+    public void testFridayCheckFourth() {
+        String actual = FridayImInLove.fridayCheck(false, false);
+        assertEquals(FC_RESULT_3, actual);
+    }
+
+    @Test
+    public void testBarTripFirst() {
         String actual = FridayImInLove.barTrip(10,20, 50);
         assertEquals(BT_RESULT_1, actual);
+    }
+
+    @Test
+    public void testBarTripSecond() {
+        String actual = FridayImInLove.barTrip(10,20, 25);
+        assertEquals(BT_RESULT_2, actual);
+    }
+
+    @Test
+    public void testBarTripThird() {
+        String actual = FridayImInLove.barTrip(10,20, 15);
+        assertEquals(BT_RESULT_3, actual);
+    }
+
+    @Test
+    public void testBarTripFourth() {
+        String actual = FridayImInLove.barTrip(10,20, 5);
+        assertEquals(BT_RESULT_4, actual);
     }
 }
